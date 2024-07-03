6d707106c839cbac3e20a69697a8bef472f70935
lucasr1b <lucascamribeiro@gmail.com>
2024-07-03 23:57:06 +1200

    Styled feed

--- src/components/feed/NewPost.tsx
+++ src/components/feed/NewPost.tsx
@@ -1,11 +1,30 @@
+'use client';
+import { useRef } from 'react';
 import Button from '../atomic/Button';
 
 const NewPost = () => {
+  const postRef = useRef<HTMLTextAreaElement>(null);
+
+  const adjustTextareaHeight = () => {
+    if (postRef.current) {
+      postRef.current.style.height = 'auto'; // Reset height to auto
+      postRef.current.style.height = `${postRef.current.scrollHeight}px`; // Set height to scrollHeight
+    }
+  };
+
   return (
-    <div className='border-2 rounded-md border-accent-secondary p-4 w-full'>
-      <div className='flex flex-row items-center gap-4 mb-4'>
-        <div className='bg-gray-400 w-10 h-10 rounded-full'></div>
-        <span className='text-xl'>What&apos;s happening?</span>
+    <div className='border-2 rounded-lg border-accent-secondary p-4 w-full relative'>
+      <div className='flex mb-4'>
+        <div className='absolute top-4 left-4'>
+          <div className='bg-gray-400 w-10 h-10 rounded-full'></div>
+        </div>
+        <textarea
+          ref={postRef}
+          className='text-xl outline-none w-full h-auto max-h-96 resize-none placeholder:text-secondary pl-14 pt-2'
+          placeholder="What's happening"
+          onChange={adjustTextareaHeight}
+        />
+
       </div>
       <Button small className='ml-auto'>Post</Button>
     </div>
